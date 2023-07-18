9b509c13c4d5a1cdcdf7c095954bb455b1169da0
Ayush bahuguna <ayushbahuguna1122@gmail.com>
2023-07-18 19:53:02 +0100

    herosection more funcanallity added

--- src/Components/Hero Section/HeroSection.jsx
+++ src/Components/Hero Section/HeroSection.jsx
@@ -27,76 +27,75 @@ export default function HeroSection(props) {
 
             <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
-
                     <div className="carousel-item active"  >
                         {Show1 ? (
-                            <>
-                                <LazyLoadImage
-                                    style={{ height: "90vh", width: "100vw", objectFit: "cover", backgroundPosition: "center" }}
-                                    alt="..."
-                                    effect="blur"
-                                    src={`https://image.tmdb.org/t/p/original/${props.HeroSectionArray1?.backdrop_path}`}
-                                    placeholderSrc={props.HeroSectionArray1?.backdrop_path}
-                                    onLoad={() => { setShow1(true) }}
-                                />
+                            <div id="herosectiondetails" style={{ position: "absolute", bottom: "10px", left: "10px", color: "white", maxWidth: "50vw", zIndex: "2", padding: "15px", backdropFilter: "blur(5px)", borderRadius: "7px" }} >
 
-                                <div id="herosectiondetails" style={{ position: "absolute", bottom: "10px", left: "10px", color: "white", maxWidth: "50vw", zIndex: "2", padding: "15px", backdropFilter: "blur(5px)", borderRadius: "7px" }} >
-
-                                    <p> <strong style={{ fontSize: "25px" }}> {props.HeroSectionArray1?.original_title}</strong> </p>
-                                    <p>{props.HeroSectionArray1?.overview?.slice(0, 100)} . . . . . .</p>
-                                    <br></br>
-                                    <div className="btn btn-primary" onClick={OnclickHandler1}>Visit Movie</div>
-                                </div>
-                            </>
+                                <p> <strong style={{ fontSize: "25px" }}> {props.HeroSectionArray1?.original_title}</strong> </p>
+                                <p>{props.HeroSectionArray1?.overview?.slice(0, 100)} . . . . . .</p>
+                                <br></br>
+                                <div className="btn btn-primary" onClick={OnclickHandler1}>Visit Movie</div>
+                            </div>
                         ) : (<Loader />)}
+
+                        <LazyLoadImage
+                            style={{ height: "90vh", width: "100vw", objectFit: "cover", backgroundPosition: "center" }}
+                            alt="..."
+                            effect="blur"
+                            src={`https://image.tmdb.org/t/p/original/${props.HeroSectionArray1?.backdrop_path}`}
+                            placeholderSrc={props.HeroSectionArray1?.backdrop_path}
+                            onLoad={() => { setShow1(true) }}
+
+                        />
+
+
 
                     </div>
 
                     <div className="carousel-item" >
                         {Show2 ? (
-                            <>
-                                <LazyLoadImage
-                                    style={{ height: "90vh", width: "100vw", objectFit: "cover", backgroundPosition: "center" }}
-                                    alt="..."
-                                    effect="blur"
-                                    src={`https://image.tmdb.org/t/p/original/${props.HeroSectionArray2?.backdrop_path}`}
-                                    placeholderSrc={props.HeroSectionArray2?.backdrop_path}
-                                    onLoad={() => { setShow2(true) }}
-                                />
 
-                                <div id="herosectiondetails" style={{ position: "absolute", bottom: "10px", left: "10px", color: "white", maxWidth: "50vw", zIndex: "2", padding: "15px", backdropFilter: "blur(5px)", borderRadius: "7px" }} >
+                            <div id="herosectiondetails" style={{ position: "absolute", bottom: "10px", left: "10px", color: "white", maxWidth: "50vw", zIndex: "2", padding: "15px", backdropFilter: "blur(5px)", borderRadius: "7px" }} >
 
-                                    <p> <strong style={{ fontSize: "25px" }}> {props.HeroSectionArray2?.original_title}</strong> </p>
-                                    <p>{props.HeroSectionArray2?.overview?.slice(0, 100)} . . . . . .</p>
-                                    <br></br>
-                                    <div className="btn btn-primary" onClick={OnclickHandler2}>Visit Movie</div>
-                                </div>
-                            </>
+                                <p> <strong style={{ fontSize: "25px" }}> {props.HeroSectionArray2?.original_title}</strong> </p>
+                                <p>{props.HeroSectionArray2?.overview?.slice(0, 100)} . . . . . .</p>
+                                <br></br>
+                                <div className="btn btn-primary" onClick={OnclickHandler2}>Visit Movie</div>
+                            </div>
                         ) : (<Loader />)}
+                        <LazyLoadImage
+                            style={{ height: "90vh", width: "100vw", objectFit: "cover", backgroundPosition: "center" }}
+                            alt="..."
+                            effect="blur"
+                            src={`https://image.tmdb.org/t/p/original/${props.HeroSectionArray2?.backdrop_path}`}
+                            placeholderSrc={props.HeroSectionArray2?.backdrop_path}
+                            onLoad={() => { setShow2(true) }}
+                        />
+
+
                     </div>
 
                     <div className="carousel-item" >
                         {Show3 ? (
-                            <>
 
-                                <LazyLoadImage
-                                    style={{ height: "90vh", width: "100vw", objectFit: "cover", backgroundPosition: "center" }}
-                                    alt="..."
-                                    effect="blur"
-                                    src={`https://image.tmdb.org/t/p/original/${props.HeroSectionArray3?.backdrop_path}`}
-                                    placeholderSrc={props.HeroSectionArray3?.backdrop_path}
-                                    onLoad={() => { setShow3(true) }}
-                                />
+                            <div id="herosectiondetails" style={{ position: "absolute", bottom: "10px", left: "10px", color: "white", maxWidth: "50vw", zIndex: "2", padding: "15px", backdropFilter: "blur(5px)", borderRadius: "7px" }} >
 
-                                <div id="herosectiondetails" style={{ position: "absolute", bottom: "10px", left: "10px", color: "white", maxWidth: "50vw", zIndex: "2", padding: "15px", backdropFilter: "blur(5px)", borderRadius: "7px" }} >
-
-                                    <p> <strong style={{ fontSize: "25px" }}> {props.HeroSectionArray3?.original_title}</strong> </p>
-                                    <p>{props.HeroSectionArray3?.overview?.slice(0, 100)} . . . . . .</p>
-                                    <br></br>
-                                    <div className="btn btn-primary" onClick={OnclickHandler3}>Visit Movie</div>
-                                </div>
-                            </>
+                                <p> <strong style={{ fontSize: "25px" }}> {props.HeroSectionArray3?.original_title}</strong> </p>
+                                <p>{props.HeroSectionArray3?.overview?.slice(0, 100)} . . . . . .</p>
+                                <br></br>
+                                <div className="btn btn-primary" onClick={OnclickHandler3}>Visit Movie</div>
+                            </div>
                         ) : (<Loader />)}
+                        <LazyLoadImage
+                            style={{ height: "90vh", width: "100vw", objectFit: "cover", backgroundPosition: "center" }}
+                            alt="..."
+                            effect="blur"
+                            src={`https://image.tmdb.org/t/p/original/${props.HeroSectionArray3?.backdrop_path}`}
+                            placeholderSrc={props.HeroSectionArray3?.backdrop_path}
+                            onLoad={() => { setShow3(true) }}
+                        />
+
+
                     </div>
 
 
